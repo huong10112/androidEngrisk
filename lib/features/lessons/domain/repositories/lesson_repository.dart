39e5296ca9e03ac0acf44869ae60fbcd11f8641b
huong10112 <huonglan10112@gmail.com>
2025-10-31 13:56:19 +0700

    Delete lib directory

--- lib/features/lessons/domain/repositories/lesson_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../entities/lesson_entity.dart';
-
-abstract class LessonRepository {
-  Future<List<LessonEntity>> getLessonsByLevel(String level);
-  Future<String> getAudioUrl(String audioKey);
-}
